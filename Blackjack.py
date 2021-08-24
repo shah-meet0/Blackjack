8c02152e48eacf94f7dba93797b4d483662936b1
shah-meet0 <flario.ms@gmail.com>
2021-08-24 15:16:46 +0100

    made a betting system

--- Blackjack.py
+++ Blackjack.py
@@ -6,15 +6,24 @@ class BlackjackGame:
     def __init__(self):
         self.player_cards = []
         self.dealer_cards = []
+        self.bet = 1
 
     def run(self):
         game_running = True
+        ini_bet = self.get_bet_amount()
+        self.bet = ini_bet
         while game_running:
-            action = input("What would you like to do? (Start, Quit)\n").lower()
+            if self.bet == 0:
+                print("Lost all your money")
+                print("Went from $" + str(ini_bet) + " to $" + str(self.bet))
+                print("Thanks for playing")
+                break
+            action = input("\nWhat would you like to do? (Start, Quit) (Money: $" + str(self.bet) + ") \n").lower()
             if action == "start":
                 self.game()
             elif action == "quit":
                 print("Thanks for playing")
+                print("Went from $" + str(ini_bet) + " to $" + str(self.bet))
                 game_running = False
             else:
                 print("Invalid Input")
@@ -24,13 +33,15 @@ class BlackjackGame:
         game_over = False
         while not game_over:
             deck.shuffle()
+            self.bet_statement()
+            bet = self.get_bet()
             self.player_cards.append(deck.draw())
             self.dealer_cards.append(deck.draw())
             do_dealer_turn = self.player_turn(deck)
             if do_dealer_turn:
                 self.dealer_turn(deck)
             deck.recombine()
-            print(self.winner_statement(self.decide_winner()))
+            print(self.winner_statement(self.decide_winner(), bet))
             self.player_cards.clear()
             self.dealer_cards.clear()
             game_over = True
@@ -72,13 +83,18 @@ class BlackjackGame:
         else:
             return "dealer"
 
-    def winner_statement(self, winner):
+    def winner_statement(self, winner, bet):
         if winner == "player":
+            self.bet += bet
             return "Congrats, you won."
         if winner == "dealer":
+            self.bet -= bet
             return "Sorry, you lost."
         if winner == "tie":
             return "Game was tied."
+
+    def bet_statement(self):
+        return "Currently have $" + str(self.bet)
 
     def get_player_value(self, dealer=False):
         value = 0
@@ -127,3 +143,31 @@ class BlackjackGame:
         else:
             print("Invalid Input")
             return BlackjackGame.get_continue_quit()
+
+    @staticmethod
+    def get_bet_amount():
+        try:
+            action = int(input("Enter initial money:\n"))
+            if action > 0:
+                return action
+            else:
+                print("Please enter positive value.")
+                return BlackjackGame.get_bet_amount()
+        except ValueError:
+            print("Invalid Input")
+            return BlackjackGame.get_bet_amount()
+
+    def get_bet(self):
+        try:
+            bet_amount = int(input("Enter amount to be bet: \n"))
+            if bet_amount > self.bet:
+                print("Betting more than you have.")
+                return self.get_bet()
+            elif bet_amount < 0:
+                print("Please bet positive amounts.")
+                return self.get_bet()
+            else:
+                return bet_amount
+        except ValueError:
+            print("Invalid Input")
+            return self.get_bet()
